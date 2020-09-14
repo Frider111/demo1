f03f59d9b837fb8f8df5bd16ec7929ad5070cd7a
abt <1042971259@qq.com>
2020-09-14 20:35:14 +0800

    first demo

--- demo-child/src/main/java/com/test/Test.java
+++ demo-child/src/main/java/com/test/Test.java
@@ -7,6 +7,7 @@ package com.test;
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println("22222222222");
         System.out.println("args = " + args);
     }
 
